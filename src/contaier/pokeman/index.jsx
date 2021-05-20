2fa20b5a81226ee3205d3a7396c0ae85b8fa4ea2
maoxiaodun <1830728154@qq.com>
2021-05-20 09:34:09 +0800

    fix: index pages changed

--- src/contaier/pokeman/index.jsx
+++ src/contaier/pokeman/index.jsx
@@ -6,6 +6,7 @@ class Contaier extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <p>测试改变部署</p>
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 200 }}>
           {baseURLs.map((item, index) => <Card baseUrl={item} isRandom={index === 3} key={index} />)}
         </div>
