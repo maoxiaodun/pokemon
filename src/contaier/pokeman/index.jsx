8931731df3eddbb4605d3d73f7b6d2d473c96862
leon <liujinwen@anewayer.com>
2022-04-24 15:42:21 +0800

    :sparkles: 新增cropper

--- src/contaier/pokeman/index.jsx
+++ src/contaier/pokeman/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { baseURLs } from './conf';
 import Card from './component/card';
+import Cropper from "react-cropper";
+import 'cropperjs/dist/cropper.css';
 
 class Contaier extends React.Component {
   render() {
@@ -10,6 +12,13 @@ class Contaier extends React.Component {
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 200 }}>
           {baseURLs.map((item, index) => <Card baseUrl={item} isRandom={index === 3} key={index} />)}
         </div>
+        <Cropper
+          style={{ height: '100vh', width: 400, }}
+          checkCrossOrigin={false}
+          highlight={false}
+          dragMode={'crop'}
+          src="https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg"
+        />
       </React.Fragment>
     )
   }
